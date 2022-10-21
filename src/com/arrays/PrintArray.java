d43939470ea66461c7538e1948822e5ee790b080
ram-221 <rameshmalli857.com>
2022-10-21 22:44:34 +0530

    Print Elements Of Array

---
+++ src/com/arrays/PrintArray.java
@@ -0,0 +1,15 @@
+package com.arrays;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		
+		int array[] = new int[] {11,22,33,44,55};
+		
+		System.out.println("Elements Of given Array");
+		
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]+" ");
+		}
+	}
+}
