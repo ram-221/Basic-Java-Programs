76f214998a506cfaa464f34eb72fc777159c32d7
ram-221 <rameshmalli857.com>
2022-10-22 07:49:49 +0530

    Check the String Empty or Not using isEmpty()

---
+++ src/com/string/IsEmpty.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class IsEmpty {
+
+	public static void main(String[] args) {
+		String str1 = "Java";
+		String str2 = " ";
+		String str3 = "";
+		System.out.println(str1.isEmpty());
+		System.out.println(str2.isEmpty());
+		System.out.println(str3.isEmpty());
+	}
+}
